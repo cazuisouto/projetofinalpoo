bfee95a8916985b09aafc26b43a68038cc3da4c9
cazui souto <116229302+cazuisouto@users.noreply.github.com>
2026-07-14 14:36:03 -0300

    CRUD usuários

--- Views/view.py
+++ Views/view.py
@@ -8,14 +8,45 @@ from datetime import datetime
 
 class View:
 
+    # OPERAÇÕES USUÁRIOS: CRUD 
+
     @staticmethod
     def listar_usuarios():
-        return UsuarioDAO().listar()
+        return UsuarioDAO().listar()    
 
     @staticmethod
     def criar_admim():
-        ...
+        for obj in View.listar_usuarios():
+            if "admin@abc.com" in obj.get_email(): return
+        View.inserir_usuario("Admin", "admin@abc.com", "admin123", "admin" )
     
     @staticmethod
     def autentica_usuario(email, senha):
-        ...
+        usuarios = UsuarioDAO().listar()
+        for info in usuarios:
+            if (info.get_email() == email) and (info.get_senha() == senha):
+                return info.get_id()
+        raise ValueError("Usuário ou senha inválidos.")
+    
+    @staticmethod
+    def inserir_usuario(nome, email, senha, funcao):
+        u= Usuario(0, nome, email, senha, funcao)
+        UsuarioDAO().inserir(u)
+
+    @staticmethod
+    def listar_usuario_id(id):
+        return UsuarioDAO().listar_id(id)
+    
+    @staticmethod
+    def atualizar_usuario(id, nome, email, senha, funcao):
+        u = Usuario(id, nome, email, senha, funcao)
+        UsuarioDAO().atualizar(u)
+
+    @staticmethod
+    def excluir_usuario(id):
+        UsuarioDAO().excluir(id)
+    
+    @staticmethod
+    def listar_usuarios_nome_parcial(nome_parcial):
+        todos_usuarios = UsuarioDAO().listar()
+        return [u for u in todos_usuarios if nome_parcial.lower() in u.get_nome().lower()]
